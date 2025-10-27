438e3c9fece59865ca08916b27b93432bd6f652e
Shashank Reddy <144030244+Shashank-Reddy-Y@users.noreply.github.com>
2025-10-27 12:27:30 +0530

    Delete using-Windsurf directory

--- using-Windsurf/dualmind_orchestrator/test.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('c:\\\\CFiles\\\\PS3-1domain\\\\using-Windsurf\\\\dualmind_orchestrator')
-from llm_client import llm_client
-
-# Test a simple LLM call
-response = llm_client.call_llm('Test prompt', 'Return only JSON: {\"test\": \"value\"}', 100)
-print('LLM Response:', repr(response))
-print('Length:', len(response) if response else 0)
